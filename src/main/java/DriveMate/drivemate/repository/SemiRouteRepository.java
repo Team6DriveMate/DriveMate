d4c7e5081ab13af2d4bc81ccb40537378a27ed37
g2hyeong <72370234+g2hyeong@users.noreply.github.com>
2024-10-11 17:24:31 +0900

    Delete src directory

--- src/main/java/DriveMate/drivemate/repository/SemiRouteRepository.java
+++
@@ -1,9 +0,0 @@
-package DriveMate.drivemate.repository;
-
-import DriveMate.drivemate.domain.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SemiRouteRepository extends JpaRepository<Route, Long> {
-}
