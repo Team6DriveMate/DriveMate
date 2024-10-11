ef3ac6d68429ea22f36767a009b1dffea8de806a
g2hyeong <72370234+g2hyeong@users.noreply.github.com>
2024-10-11 17:34:56 +0900

    Basic Domain, Repository and Connecting OpenAPI
    Connected with TMap route-offering api

---
+++ BE/drivemate/src/main/java/DriveMate/drivemate/repository/RouteRepository.java
@@ -0,0 +1,9 @@
+package DriveMate.drivemate.repository;
+
+import DriveMate.drivemate.domain.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
