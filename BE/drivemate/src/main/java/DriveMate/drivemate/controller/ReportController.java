6d0b8a2a25afaa9ab68140721fb51eedab6a4eaa
g2hy <g2hyeong@gmail.com>
2024-12-02 17:10:30 +0900

    + read 오류 수정

--- BE/drivemate/src/main/java/DriveMate/drivemate/controller/ReportController.java
+++ BE/drivemate/src/main/java/DriveMate/drivemate/controller/ReportController.java
@@ -522,7 +522,7 @@ public class ReportController {
         return driveReportRespondDTO;
     }
 
-    @PostMapping("/read")
+    @PostMapping("/read") // 경험치
     public SuccessRespondDTO checkReport(){
         SuccessRespondDTO successRespondDTO = new SuccessRespondDTO();
         try {
